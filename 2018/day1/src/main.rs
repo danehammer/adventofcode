cbcebdd0f21602c65bfd7de92c72c647076ae540
Dane Hammer <dane.molotok@gmail.com>
2018-12-01 12:42:36 -0600

    chore: refactor list of lines init

--- 2018/day1/src/main.rs
+++ 2018/day1/src/main.rs
@@ -5,11 +5,12 @@ use std::fs::File;
 fn main() {
     let f = File::open("input.txt").unwrap();
     let file = BufReader::new(&f);
+    let lines = file.lines().enumerate();
 
     // Initialize frequency at zero per the instructions
     let mut freq = 0;
 
-    for (_num, line) in file.lines().enumerate() {
+    for (_num, line) in lines {
         let l = line.unwrap();
         if l == "0" || l == "" {
             continue;
