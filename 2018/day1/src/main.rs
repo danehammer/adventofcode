a0353353224bcf32e6eecfb300bc6991dadfd2bd
Dane Hammer <dane.molotok@gmail.com>
2018-12-01 13:57:06 -0600

    day1 part2 solution (probably bad perf)

--- 2018/day1/src/main.rs
+++ 2018/day1/src/main.rs
@@ -1,24 +1,40 @@
 use std::io::BufReader;
 use std::io::BufRead;
 use std::fs::File;
+use std::vec::Vec;
 
 fn main() {
-    let f = File::open("input.txt").unwrap();
-    let file = BufReader::new(&f);
-    let lines = file.lines().enumerate();
-
     // Initialize frequency at zero per the instructions
     let mut freq = 0;
+    let mut freqs_seen = Vec::new();
+    freqs_seen.push(freq);
 
-    for (_num, line) in lines {
-        let l = line.unwrap();
-        if l == "0" || l == "" {
-            continue;
+    'outer: loop {
+        let f = File::open("input.txt").unwrap();
+        let file = BufReader::new(&f);
+        let lines = file.lines();
+
+        for (_, line) in lines.enumerate() {
+            let l = line.unwrap();
+            if l == "0" || l == "" {
+                continue;
+            }
+            let freq_change = l.parse::<i32>().unwrap();
+            
+            freq = freq + freq_change;
+
+            // Check if we've seen this frequency before
+            let r = freqs_seen.binary_search(&freq);
+            if r.is_ok() {
+                println!("Repeat frequency found: {}", freq);
+                break 'outer;
+            }
+
+            // Otherwise note that we've seen it
+            freqs_seen.push(freq);
+            // Re-sort
+            freqs_seen.sort_unstable();
         }
-        let freq_change = l.parse::<i32>().unwrap();
-        
-        freq = freq + freq_change;
+        println!("{}", freqs_seen.len());
     }
-    
-    println!("Final freq: {}", freq);
 }
